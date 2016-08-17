ac7887c134038b43dd237e3107c3b4483c61bc75
wapdeveloper <xspangrui@outlook.com>
2016-08-17 21:37:00 +0800

    add-pcg-

--- draw-bundle/Reload.m
+++ draw-bundle/Reload.m
@@ -53,7 +53,22 @@ time=[time  time1];
 values=[values values1];
 
 
+time1=load('pcg-b-time');
+values1=load('pcg-b-value');
+n=size(time1,1);
+A=zeros(1,50-n);
+for a = 1:50-n
+  A(1,a)=NaN;
+end
+% 补全矩阵对齐
+merge=[time1' A];
+time1=merge';
+merge=[values1' A];
+values1=merge';
 
+% 合并前两列
+time=[time  time1];
+values=[values values1];
 
 
 
